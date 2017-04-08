a8e11ce334ce1d74a7a9a3552da96e57cac6f5dd
sirwilliamiv <wjconnolly2@gmail.com>
2017-04-08 11:02:35 -0500

    added drop table to heroes exports.down

--- 8a-knex-migration/knex-sandcastle/migrations/20170408105135_heroes.js
+++ 8a-knex-migration/knex-sandcastle/migrations/20170408105135_heroes.js
@@ -2,10 +2,13 @@
 exports.up = function(knex, Promise) {
   return knex.schema
   .createTable('heroes', function(table){
-
+    table.increments('hero_id');
+    table.string('hero_name').notNullable();
+    table.string('hero_weapon').notNullable();
   })
 };
 
 exports.down = function(knex, Promise) {
+  return knex.schema.dropTable('heroes');
 
 };
